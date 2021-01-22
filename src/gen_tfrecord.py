2bc17a9f1fbcb97e7925a881d5413107c5cb06f9
Juan Sebastian Maldonado <maldonadojsm@gmail.com>
2021-01-22 12:21:25 +0100

    Update gen_tfrecord.py

--- src/gen_tfrecord.py
+++ src/gen_tfrecord.py
@@ -50,7 +50,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path, label_map):
-    with tf.gfile.GFile(os.path.join(path, "{}".format(group.filename)),
+    with tf.compat.v1.gfile.GFile(os.path.join(path, "{}".format(group.filename)),
                         "rb") as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
@@ -136,4 +136,4 @@ def main(_):
 
 
 if __name__ == "__main__":
-    tf.app.run()
+    tf.compat.v1.app.run()
